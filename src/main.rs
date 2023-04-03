1e9a121bd9d9f886e82497f911a57e016d437c3f
knapptr <knapptr@gmail.com>
2023-04-03 08:40:51 -0400

    Removes unnecessary println(s)

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use screen::{Render, Screen};
 
 fn main() {
     let mut grid = Grid::new(50, 0.3);
-    println!("{}", grid);
-    println!("{}", grid);
     let screen = Screen::new(4);
     let mut count = 0;
     screen.do_loop(|| {
